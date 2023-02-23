514c4f90017dfa2f052837f9567691411e7930e6
n30h3r0 <83586161+n30h3r0@users.noreply.github.com>
2023-02-23 11:19:00 -0300

    Add files via upload

---
+++ sg.tf
@@ -0,0 +1,16 @@
+data "aws_security_group" "default" {
+  filter {
+    name   = "group-name"
+    values = ["default"]
+  }
+}
+
+resource "aws_security_group_rule" "trafego_porta_80" {
+  security_group_id = data.aws_security_group.default.id
+
+  type        = "ingress"
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  from_port   = 80
+  to_port     = 80
+}
